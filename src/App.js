d4eb025301f4c1fa46dbccce9bc264721befd660
sajadmsNew <sajad.muosavi@factmata.com>
2021-12-15 00:57:26 +0330

    router implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './index.css';
 import Page1 from './pages/page1';
 import Page2 from './pages/page2';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Page2 />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Page2/>} />
+          <Route path="/dentist" element={<Page1/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
