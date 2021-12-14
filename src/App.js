771671dbada2e40cca732d3c58fa95dc79052be0
sajadmsNew <sajad.muosavi@factmata.com>
2021-12-15 01:34:25 +0330

    page3 added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './index.css';
-import Page1 from './pages/page1';
-import Page2 from './pages/page2';
+// import Page1 from './pages/page1';
+// import Page2 from './pages/page2';
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
 import React from 'react';
+import Page3 from './pages/pages3';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path="/" element={<Page2/>} />
-          <Route path="/dentist" element={<Page1/>} />
+          {/* <Route exact path="/" element={<Page2/>} />
+          <Route path="/dentist" element={<Page1/>} /> */}
+          <Route path="/pt" element={<Page3/>} />
         </Routes>
       </Router>
     </div>
