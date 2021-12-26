92374bff03f26c370d43bbecffcaea2017a5d61b
sajadmsNew <sajad.muosavi@factmata.com>
2021-12-27 02:30:21 +0330

    lazy load routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import './index.css';
-// import Page1 from './pages/page1';
-// import Page2 from './pages/page2';
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
-import React from 'react';
-// import Page3 from './pages/pages3';
-import Page4 from './pages/page4';
+import React, { Suspense, lazy } from 'react';
+import './index.css';
+const Page1 = lazy(() => import('./pages/page1'));
+const Page2 = lazy(() => import('./pages/page2'));
+const Page3 = lazy(() => import('./pages/pages3'));
+const Page4 = lazy(() => import('./pages/page4'));
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          {/* <Route exact path="/" element={<Page2/>} />
-          <Route path="/dentist" element={<Page1/>} /> */}
-          {/* <Route path="/pt" element={<Page3/>} /> */}
-          <Route path="/lab" element={<Page4/>} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route exact path="/" element={<Page2 />} />
+            <Route path="/dentist" element={<Page1 />} />
+            <Route path="/pt" element={<Page3 />} />
+            <Route path="/lab" element={<Page4 />} />
+          </Routes>
+        </Suspense>
       </Router>
     </div>
   );
